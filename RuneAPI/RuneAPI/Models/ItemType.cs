8fe4a1e44eee7ee235a4e03275f0a06d0018a078
Philip Persson <philipp.dev@hotmail.com>
2021-11-06 16:53:06 +0100

    Fixed itemType values

--- RuneAPI/RuneAPI/Models/ItemType.cs
+++ RuneAPI/RuneAPI/Models/ItemType.cs
@@ -5,22 +5,22 @@ namespace RuneAPI.Models
     [Flags]
     public enum ItemType
     {
-        Sword,
-        Axe,
-        Mace,
-        Staff,
-        MeleeWeapon,
-        BowsCrossbow,
-        Scepter,
-        Shield,
-        Club,
-        Hammer,
-        Wand,
-        Polearm,
-        RangedWeapon,
-        Claws,
-        BodyArmor,
-        PaladinShield,
-        Helmet
+        Sword = 1,
+        Axe = 2,
+        Mace = 4,
+        Staff = 8,
+        MeleeWeapon = 16,
+        BowsCrossbow = 32,
+        Scepter = 64,
+        Shield = 128,
+        Club = 256,
+        Hammer = 512,
+        Wand = 1024,
+        Polearm = 2048,
+        RangedWeapon = 4096,
+        Claws = 8192,
+        BodyArmor = 16384,
+        PaladinShield = 32768,
+        Helmet = 65536
     }
 }
